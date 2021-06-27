a72fa01102b76eba543cd8a663b012f1ac0630ec
Natan <natan-moreira1@hotmail.com>
2021-06-27 14:08:44 -0300

    feat: create authentication token service

---
+++ app/services/authentication_token_service.rb
@@ -0,0 +1,16 @@
+class AuthenticationTokenService
+  @token_conf = {
+    secret:'my$ecretK3y',
+    algorithm: 'HS256'
+  }
+  def self.encode(user_id)
+    exp = (Time.now + 30.minutes).to_i
+    payload = {user_id: user_id, exp: exp}
+    JWT.encode payload, @token_conf[:secret], @token_conf[:algorithm]
+  end
+
+  def self.decode(token)
+    decoded_token = JWT.decode token, @token_conf[:secret], true, {algorithm: @token_conf[:algorithm]}
+    decoded_token[0]
+  end
+end
