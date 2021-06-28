b77b5df208ff6cd924951947dbc7687642e0907f
Natan <natan-moreira1@hotmail.com>
2021-06-28 16:31:05 -0300

    feat: create WebCrawlerService

---
+++ app/services/web_crawler_service.rb
@@ -0,0 +1,41 @@
+require "nokogiri"
+require "open-uri"
+
+class WebCrawlerService
+
+  attr_reader :tag
+
+  def initialize(tag)
+    @tag = tag 
+  end
+
+  def results
+    doc = Nokogiri::HTML(URI.open(path))
+    find_info(doc)
+  end
+
+  private 
+
+  def find_info(doc)
+    results = []
+    doc.search("div .quote").each do |obj|
+  
+      results << process_obj(obj)
+    end
+    results 
+  end
+
+  def process_obj(obj)
+    {
+      text: obj.search("span.text").text,
+      author_name: obj.at("span small.author").text,
+      link_author: PATH + obj.at("span a[href]")["href"],
+      tags: obj.at_css("div.tags meta.keywords")["content"].split(",")
+    } 
+  end
+
+  def path(tag)
+    "http://quotes.toscrape.com/tag/#{@tag}/page/1/"
+  end
+
+end
